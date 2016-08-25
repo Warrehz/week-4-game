7b45167d0ccf398db85950a29cbb8c739e721d09
Mical Juarez <mical.juarez@gmail.com>
2016-08-25 02:36:32 -0500

    added some html, minor css and javascript logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+var characters = [];
+
+characters[0] = new Image();
+characters[0].src = "../images/contra-bill.png";
+
+characters[1] = new Image();
+characters[1].src = "../images/link.png";
+
+characters[2] = new Image();
+characters[2].src = "../images/mario.png";
+
+characters[3] = new Image();
+characters[3].src = "../images/megaman.png";
+
+characters[4] = new Image();
+characters[4].src = "../images/samus.png";
+
+characters[5] = new Image();
+characters[5].src = "../images/simon-belmont.png";
